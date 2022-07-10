d6ca2ad37a98636590548961b0fbc1d31527897f
Filipe Magalhães Lima <filipi99ff@gmail.com>
2022-07-10 19:13:29 -0300

    git commit -m "Lista de exercícios"

--- src/br/com/Exercicio/PessoaJuridicaLuana.java
+++ src/br/com/Exercicio/PessoaJuridicaLuana.java
@@ -15,9 +15,9 @@ public class PessoaJuridicaLuana {
             System.out.print("-Informe o seu e-mail: ");
             String email = sc.nextLine();
             System.out.print("-Fale a sua idade: ");
-            Integer idade = sc.nextInt();
+            int idade = sc.nextInt();
             System.out.print("-Diga o seu ID: ");
-            Integer id = sc.nextInt();
+            int id = sc.nextInt();
             PessoaJuridica PJ = new PessoaJuridica(nome, idade, email);
 
         System.out.println("#"+id+" O seu nome é: "+PJ.getNome()+" o seu e-mail é: " + PJ.getEmail() + " e a sua idade é: " + PJ.getIdade());
